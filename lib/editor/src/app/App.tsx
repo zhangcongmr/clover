7cf7e8911666ed96abb2469a51c3e40af0731421
zc <cong@example1.com>
2026-01-21 23:27:58 +0800

    fix: change conf and add assistant app
    Signed-off-by: zc <cong@example1.com>

--- lib/editor/src/app/App.tsx
+++ lib/editor/src/app/App.tsx
@@ -1,5 +1,35 @@
-import { ApiTester } from '@/app/components/ApiTester';
+import { useEffect } from 'react';
+import { loadAstApp } from 'assistant';
+
+// 防止重复加载
+let angularLoaded = false;
 
 export default function App() {
-  return <ApiTester />;
+
+  useEffect(() => {
+    if (angularLoaded) return;
+
+
+    angularLoaded = true;
+
+    loadAstApp()
+      .then(start => start({ apiUrl: '...' }))
+      .catch(err => {
+        console.error('Angular failed to start', err);
+        angularLoaded = false; // 可选：允许重试
+      });
+
+    return () => {
+      // 可选：清理逻辑（通常不需要移除 script）
+    };
+  }, []);
+
+  return (
+    <div>
+      {/* <h1>React App</h1> */}
+      {/* 使用 Angular Web Component */}
+      <app-root style={{ height: '100vh', display: 'block' }}></app-root>
+      {/* <p>Back to React</p> */}
+    </div>
+  );
 }
--- lib/editor/vite.config.ts
+++ lib/editor/vite.config.ts
@@ -4,11 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', //Added base configuration  确保编译后index.html中资源引用为 <script src="./assets/script.js"></script>， 而不是<script src="/assets/script.js"></script>
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
     react(),
-    tailwindcss(),
+    tailwindcss()
   ],
   resolve: {
     alias: {
