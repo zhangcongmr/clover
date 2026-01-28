211046cfd4ed7736291c1708e0ae9b9e1e88b87a
zc <cong@example1.com>
2026-01-28 19:35:54 +0800

    fix: remove unused code
    Signed-off-by: zc <cong@example1.com>

--- lib/community-widget/src/app/App.tsx
+++ lib/community-widget/src/app/App.tsx
@@ -2,8 +2,6 @@ import { Header } from '@/app/components/Header';
 import { CategorySidebar } from '@/app/components/CategorySidebar';
 import { MainContent } from '@/app/components/MainContent';
 import { TrendingProjects } from '@/app/components/TrendingProjects';
-import { coreService } from './core.service';
-import { useEffect, useState } from 'react';
 
 interface MyReactComponentProps {
   baseHref?: string
@@ -13,40 +11,6 @@ interface MyReactComponentProps {
 export default function App({baseHref, onAction} : MyReactComponentProps) {
   baseHref = baseHref == null?"" : baseHref;
   baseHref = baseHref.replace(/\/+$/, '');  //去掉末尾的/
-
-  let data: any = [];
-  const [rawSpecDef, setRawSpecDef] = useState({});
-
-  const importFromApiDef = (rawSpecBrief: any) => {
-    fetch("https://127.0.0.1:8980/user/apiInfoModel/" + rawSpecBrief.id).then(
-      (response: any) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok.');
-        }
-        return response.json()
-      }).then(rawData => {
-        if (!rawData) {
-          return;
-        }
-        const parseOpenApiSpec = JSON.parse(rawData.profile);
-        setRawSpecDef(parseOpenApiSpec);
-        if(parseOpenApiSpec['dataType'] == 'projectType') {
-          data = parseOpenApiSpec['children'] || [];
-        } else {
-          data = coreService.parseOpenApiSpec(parseOpenApiSpec);
-        }
-
-        // 调用从 Web Component 传入的回调
-        if (onAction) {
-          onAction(data);
-        }
-    });
-  }
-
-
-  const view = (rawSpecBrief: any) => {
-  }
-
 
   return (
     <div className="h-full flex flex-col bg-gray-50">
