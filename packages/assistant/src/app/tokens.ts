f4bf66f8d56676a78e4a16ed97631a76f59a8716
zc <cong@example1.com>
2026-08-07 14:34:28 +0800

    fix: remove console log
    Signed-off-by: zc <cong@example1.com>

--- packages/assistant/src/app/tokens.ts
+++ packages/assistant/src/app/tokens.ts
@@ -3,4 +3,3 @@ import { InjectionToken } from '@angular/core';
 import { MyConfig } from './my-config.service';
 
 export const MY_CONFIG = new InjectionToken<MyConfig>('MyConfig');
-console.log('MY_CONFIG token created:', MY_CONFIG);
